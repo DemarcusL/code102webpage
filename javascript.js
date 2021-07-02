4e57e3cea48897ad9b14eb7a081771de5081338f
Demarcus <demarcuslester12@gmail.com>
2021-07-02 04:47:03 -0500

    added java to page and java page

--- javascript.js
+++ javascript.js
@@ -11,9 +11,37 @@ function guessingGame(){
   var name = prompt("Who is the main character of the anime Naruto?");
   var guessedCorrectly = checkName(name);
     if(guessedCorrectly){
-      alert("Correct Name Guessed!")
+      alert("You used summoning jutsu to gather that correct name!")
     }
     else{
-      alert("Incorrect Name, Try Again!")
+      alert("Incorrect Shinobi, Try Again!")
     }
+}
+
+for (i = 0; i < 15; i = i+2){
+    console.log(i);
+}
+
+attempts = 0;
+
+while(attempts < 3){
+  prompt("Enter a tail number");
+  attempts = attempts + 1
+}
+
+function bestfriendName(guessedName){
+    if(guessedName == "Sasuke")
+    return true;
+   }
+
+
+function bestfriendName(){
+    var name = text("What is Naruto's bestfriend name")
+    var guessedCorrectly = checkName(Sasuke);
+      if(guessedCorrectly){
+        alert("You have summoned the correct Shinobi.")
+      }
+      else{
+        alert("You have made fanboys cry today, Try Again")
+  }
 }
